b3e4639722a245171e9797725fe32426795d08e2
Hannah Rossiter <hannah.rossiter@touchnetix.com>
2023-06-15 10:35:17 +0100

    Create_GPIO_Controls_File
    u48_GPIOControls.py
    -used uXX_Templates as a base
    -created usage revisions 7, 8, and 9
    
    __init__.py
    -added u48_GPIOControls to the list of imports
    -added u48_GPIOControls to the __all__ entry

--- axiom_tc/__init__.py
+++ axiom_tc/__init__.py
@@ -4,5 +4,6 @@ from .u02_SystemManager import *
 from .u06_SelfTest import *
 from .u07_LiveView import *
 from .u33_CRCData import *
+from .u48_GPIOControls import *
 
-__all__ = ["axiom", "CDU_Common", "u02_SystemManager", "u06_SelfTest", "u07_LiveView", "u33_CRCData"]
+__all__ = ["axiom", "CDU_Common", "u02_SystemManager", "u06_SelfTest", "u07_LiveView", "u33_CRCData", "u48_GPIOControls"]
